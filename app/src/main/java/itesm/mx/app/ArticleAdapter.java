fcd95b79b2e7bfde860bda94ffe0b336be917b0d
gomezhyuuga <gomezhyuuga@gmail.com>
2015-03-06 00:42:14 -0600

    Adapter

--- app/src/main/java/itesm/mx/app/ArticleAdapter.java
+++ app/src/main/java/itesm/mx/app/ArticleAdapter.java
@@ -4,27 +4,49 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by gomezhyuuga on 05/03/15.
  */
 public class ArticleAdapter extends BaseAdapter {
+
+    List<Article> articleList = getDataForListView();
+    private String[] initVals = {"Papel", "Aluminio", "Carton", "Equipos de cómputo",
+            "Metales", "Baterías", "Envase"};
+
     @Override
     public int getCount() {
-        return 0;
+        return articleList.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return null;
+        return articleList.get(position);
     }
 
     @Override
     public long getItemId(int position) {
-        return 0;
+        return position;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         return null;
+    }
+
+    public List<Article> getDataForListView() {
+        List<Article> codeLearnChaptersList = new ArrayList<Article>();
+
+        for (String s : initVals) {
+            Article a = new Article();
+            a.name = s;
+            a.quantity = 1;
+            articleList.add(a);
+        }
+
+        return codeLearnChaptersList;
+
     }
 }
